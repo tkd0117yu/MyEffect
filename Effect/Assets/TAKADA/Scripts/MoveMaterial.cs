f987494ad01c50e2855806be7635d7a01e5c262e
741PC-17\student <uroncha.15.3@gmail.com>
2019-08-28 15:01:54 +0900

    吸い込み

---
+++ Effect/Assets/TAKADA/Scripts/MoveMaterial.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMaterial : MonoBehaviour
+{
+	void Update()
+	{
+		float scroll = Mathf.Repeat(Time.time * 0.5f, 1);
+		Vector2 offset = new Vector2(scroll, 0);
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
