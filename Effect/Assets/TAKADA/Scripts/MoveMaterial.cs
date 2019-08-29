459dfdebacf9fe3988d6586c81045537181e9936
741PC-28\student <cu.common@gmail.com>
2019-08-29 12:34:53 +0900

    作業

--- Effect/Assets/TAKADA/Scripts/MoveMaterial.cs
+++ Effect/Assets/TAKADA/Scripts/MoveMaterial.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class MoveMaterial : MonoBehaviour
 {
+	public float spead = 0.5f;
 	void Update()
 	{
-		float scroll = Mathf.Repeat(Time.time * 0.5f, 1);
-		Vector2 offset = new Vector2(scroll, 0);
+		float scroll = Mathf.Repeat(Time.time * spead, 1);
+		Vector2 offset = new Vector2(0, scroll);
 		GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
 	}
 }
